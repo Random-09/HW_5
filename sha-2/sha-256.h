6f3ec06cbd40f006b1f98fb9113d28e26030e831
kvasa <kvasan2104@gmail.com>
2023-04-28 16:53:56 +0300

    added db folder

--- sha-2/sha-256.h
+++ sha-2/sha-256.h
@@ -55,7 +55,7 @@ void calc_sha_256(uint8_t hash[SIZE_OF_SHA_256_HASH], const void *input, size_t 
  * @param hash Hash array, where the result will be delivered.
  *
  * @note If all of the data you are calculating the hash value on is not available in a contiguous buffer in memory, this is
- * where you should start. Instantiate a SHA-256 structure, for instance by simply declaring it locally, make your hash
+ * where you should start. Instantiate a SHA-256 structure, for database by simply declaring it locally, make your hash
  * buffer available, and invoke this function. Once a SHA-256 hash has been calculated (see further below) a SHA-256
  * structure can be initialized again for the next calculation.
  *
